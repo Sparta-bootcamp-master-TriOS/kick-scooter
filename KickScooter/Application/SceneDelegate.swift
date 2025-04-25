ca056a36202c8912d74109eecbb15ce252e5a3d3
JISUNG PARK <meowbutlerdev@gmail.com>
2025-04-25 18:41:39 +0900

    🔧 Config: 라이브러리 추가

--- KickScooter/Application/SceneDelegate.swift
+++ KickScooter/Application/SceneDelegate.swift
@@ -7,7 +7,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
 
-        let viewController = SignInViewController()
+        let viewController = MainViewController()
 
         window.rootViewController = viewController
         window.makeKeyAndVisible()
--- KickScooter/Presentation/Scene/Main/ViewController/MainViewController.swift
+++ KickScooter/Presentation/Scene/Main/ViewController/MainViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class SignInViewController: UIViewController {
+class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
